a93fb267d588fb0bdb3274681423f0f2f2a6a2ba
unknown <fabiansalinas99@hotmail.com>
2020-07-09 00:25:40 -0300

    Curso UTN FRA

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Salinas Gonza Fabian");
+	alert("Gonzalo Fabian Salinas");
 }
 
