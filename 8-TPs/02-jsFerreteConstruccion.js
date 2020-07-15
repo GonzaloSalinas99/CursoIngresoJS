bbc5ca73a7413b2a6708c7afe8955f1a60cbac52
unknown <fabiansalinas99@hotmail.com>
2020-07-14 23:29:25 -0300

    Ejercicio 2 del tp

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -18,19 +18,44 @@ function Rectangulo ()
 
 	calculo = (largo * 2 + ancho * 2)*3;
 
-	alert(calculo);
+	alert("la cantidad de alambre que se debe comprar es de " + calculo + " metros");
 }
 function Circulo () 
 {
 	var radio;
 	var calculo;
+	var resultado;
+	var mathPi;
 
 	radio = txtIdRadio.value;
 	radio = parseInt(radio);
+	mathPi= 3.1416
 
-	
+	calculo = (mathPi*2)* radio;
+	resultado= calculo*3;
+
+	alert("la cantidad de alambre que se debe comprar es de " + resultado + " metros");
+
 }
 function Materiales () 
 {
-	
+	var largo;
+	var ancho;
+	var calculo1;
+	var calculo2;
+	var cemento;
+	var cal;
+	largo = txtIdLargo.value;
+	ancho = txtIdAncho.value;
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+	cemento=2;
+	cal=3;
+
+	calculo1 = (largo * ancho) * cemento;
+	calculo2 = (largo * ancho) * cal;
+
+	alert("se necesitan " + calculo1 + " bolsas de cemento y " + calculo2 + (" bolsas de cal"));
 }
