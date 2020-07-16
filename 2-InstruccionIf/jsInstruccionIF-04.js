021b95440839702197318fbc6a2bb4835515b8f8
unknown <fabiansalinas99@hotmail.com>
2020-07-15 22:17:17 -0300

    nuevo avanze con IF

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,22 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad;
 
+	edad = txtIdEdad.value;
+	edad = parseInt (edad);
 
-}//FIN DE LA FUNCIÓN
+	if(edad>12 && edad<18)
+	{
+		alert ("usted es adolescente");
+	}
+
+}// o tambien se puede utilizar 
+	//if(edad>12)
+	//   if(edad < 18)
+
+		// alert ("usted es adolescente")
+
+		
+		// entonces lo que hace es que primero evalua la condicion y si se ejecuta, pasa al otro if 
+		// entonces si tambien se cumple la condicion del segundo if, se muestra el mensaje. 
+		// pero si no se cumpliera el primer if, no pasaria al segundo.
