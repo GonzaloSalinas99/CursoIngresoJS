267646275df7af905772c83c3e70f8204dbaa037
GonzaloSalinas99 <salinasgonzalo99@gmail.com>
2020-07-21 12:27:45 -0300

    Ultima modificacion de if

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -6,13 +6,19 @@ function mostrar()
 	edad = txtIdEdad.value;
 	estadoCivil = document.getElementById("estadoCivil").value;
 
-	if (edad < 18 && estadoCivil != "Soltero")
+	if(edad>17 && estadoCivil== "Soltero")
+		{
+			alert ("es soltero y no es menor");
+		}
+}
+
+	/*if (edad < 18 && estadoCivil != "Soltero")
 	{
 	
 	}
 	else if(estadoCivil== "Soltero" && edad> 17)
 	{
 		alert ("Es soltero y no es menor");
-	}
-}//FIN DE LA FUNCIÓNAl ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+	}*/
+//FIN DE LA FUNCIÓNAl ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
 // pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'
